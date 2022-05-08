61b143c4940041b41ec70d080fb83b50165c2577
Adane-M <98428193+Adane-M@users.noreply.github.com>
2022-05-08 17:58:08 +0300

    hackaton
    add a node js and some fetch methodes to restore the player name and scores

---
+++ HACKATON#2/playersData/db.js
@@ -0,0 +1,33 @@
+const knex = require('knex');
+
+const db = knex({
+  client: 'pg',
+  connection: {
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    passward: process.env.DB_PASS,
+    database: process.env.DB_NAME
+  }
+});
+
+function createUser(user_name) {
+  return db('users')
+  .insert({user_name:user_name,})
+    .returning('*')
+}
+
+function updateScore(user_name , score ) {
+  return db('users')
+    .update('score', score)
+    .where({ user_name: user_name })
+    .returning('*')
+}
+
+module.exports = {
+  createUser,
+  updateScore,
+};
+
+
+
---
+++ HACKATON#2/server.js
@@ -0,0 +1,80 @@
+const exp = require('express');
+const knex = require('knex');
+const dotenv = require('dotenv');
+const { createUser, updateScore } = require('./playersData/db.js')
+
+const app = exp();
+dotenv.config();
+app.listen(process.env.PORT, () => {
+    console.log(`listen on port ${process.env.PORT}`);
+})
+
+app.use('/', exp.static(__dirname + '/PacMan'));
+app.use(exp.urlencoded({ extended: true }));
+app.use(exp.json());
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/pacman/index.html')
+})
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: 'localhost',
+        port: '5432',
+        user: 'postgres',
+        password: 'romy2401',
+        database: 'hackaton#2'
+    }
+})
+
+//creat new player
+app.post('/player', (req, res) => {
+    // console.log(req.body);
+    createUser(req.body)
+        .then(data => {
+            console.log(data);
+            res.send({ message: 'OK' })
+        })
+        .catch(err => {
+            console.log(err);
+            res.json({ message: err.message })
+        })
+})
+
+//update player score
+app.put('/player/score', (req, res) => {
+    const { user_name, score } = req.body;
+    updateScore(user_name, score)
+        .then(data => {
+            console.log(data);
+            res.send({ message: 'OK' })
+        })
+        .catch(err => {
+            console.log(err);
+            res.json({ message: err.message })
+        })
+
+})
+
+//display top scores
+app.get('/player/best_scores', (req, res) =>{
+    db('users')
+        .select('user_name', 'score')
+        .then(data => {
+            console.log(data);
+            res.send(data)
+        })
+        .catch(err => {
+            console.log(err);
+            res.send({ message: err.detail })
+        })
+})
+
+
+
+
+
+
+
